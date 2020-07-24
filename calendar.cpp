6393e4705b01f1b791e11e528a9e5b4af8efca20
m27bay <mickael.ledenmat@gmail.com>
2020-07-24 19:02:08 +0200

    Finish print 1 month

--- calendar.cpp
+++ calendar.cpp
@@ -2,23 +2,88 @@
 
 #include <ctime>
 
-void printCalendar( std::string month, std::string dayWeek[7], int year, std::string startDay )
+void printMonth( std::string month, std::string dayWeek[7], int year, std::string startDay, int numDay, int posDay )
 {
-  std::cout << "------------------------------" << std::endl;
-  std::cout << "         ";
-  std::cout << month;
-  std::cout << " , ";
-  std::cout << year << std::endl;
-  std::cout << "------------------------------" << std::endl;
   for( int i = 0; i < 7; i++)
   {
     std::cout << " ";
     std::cout << dayWeek[i];
   }
   std::cout << "" << std::endl;
+
+  for( int i = 0; i < 7; i++)
+  {
+    if( dayWeek[i].compare(startDay) != 0 )
+    {
+      std::cout << "  ";
+    }
+    else
+      break;
+    std::cout << "  ";
+  }
+
+  std::string tmpDay = startDay;
+  int posDayTmp = posDay;
+
+  for( int i = 0; i < numDay; i++ )
+  {
+    std::cout << "  ";
+    if( tmpDay.compare("Sat") == 0 )
+    {
+      if( i+1 < 10 )
+        std::cout << "0" << i+1;
+      else
+        std::cout << i+1;
+
+      std::cout << "" << std::endl;
+      tmpDay = "Sun";
+      posDayTmp = 0;
+
+      continue;
+    }
+
+    if( i+1 < 10 )
+      std::cout << "0" << i+1;
+    else
+      std::cout << i+1;
+
+    posDayTmp++;
+    tmpDay = dayWeek[posDayTmp];
+  }
 }
 
-std::string startWeek ( std::string dayWeek[7], int day, int pos )
+int posDay( std::string day, std::string dayWeek[7] )
+{
+  for( int i = 0; i < 7; i++ )
+  {
+    if( day.compare( dayWeek[i] ) == 0 )
+      return i;
+  }
+}
+
+void printCalendar( std::string months[12], int numMonth, std::string dayWeek[7], int year, std::string startDay, int posDay )
+{
+  std::cout << "\n         Calendar" << std::endl;
+  std::cout << "------------------------------" << std::endl;
+  std::cout << "         ";
+  std::cout << months[numMonth];
+  std::cout << " , ";
+  std::cout << year << std::endl;
+  std::cout << "------------------------------" << std::endl;
+
+  if( numMonth%2 == 0 || months[numMonth].compare("Aug") == 0 )
+    printMonth( months[numMonth], dayWeek, year, startDay, 31, posDay);
+  else if( months[numMonth].compare("Feb") == 0 && year%4 == 0 )
+    printMonth( months[numMonth], dayWeek, year, startDay, 29, posDay);
+  else if( months[numMonth].compare("Feb") == 0 && year%4 != 0 )
+    printMonth( months[numMonth], dayWeek, year, startDay, 28, posDay);
+  else
+    printMonth( months[numMonth], dayWeek, year, startDay, 30, posDay);
+
+  std::cout << "" << std::endl;
+}
+
+std::string startWeek( std::string dayWeek[7], int day, int pos )
 {
   int tmp(0);
   if( day > 7 )
@@ -28,13 +93,10 @@ std::string startWeek ( std::string dayWeek[7], int day, int pos )
 
   std::string dayTmp = dayWeek[ pos - tmp + 1];
   return dayTmp;
-
 }
 
 int main(int argc, char const *argv[])
 {
-  std::cout << "\n         Calendar" << std::endl;
-
   std::string dayWeek[7] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
   std::string months[12] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
 
@@ -56,7 +118,8 @@ int main(int argc, char const *argv[])
 
   // std::cout << startWeek ( dayWeek, day, ltm->tm_wday ) << std::endl;
 
-  printCalendar( months[ltm->tm_mon], dayWeek, 1900 + ltm->tm_year, startWeek ( dayWeek, day, ltm->tm_wday ) );
+  std::string start = startWeek( dayWeek, day, ltm->tm_wday );
+  printCalendar( months, ltm->tm_mon, dayWeek, 1900 + ltm->tm_year, start, posDay( start, dayWeek ) );
 
   return 0;
 }
