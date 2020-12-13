02504725f0917d6d5194ba03a8732a8a1d64ff7f
m27bay <mickael.ledenmat@gmail.com>
2020-12-13 21:09:19 +0100

    tray iteratif : fail

--- td_lisp/entrainement.lisp
+++ td_lisp/entrainement.lisp
@@ -6,14 +6,45 @@
     )
 )
 
+(format t "~%##### test tailleListe #####~%")
 (format t "(tailleListe '(1 2 3 4) : ~A ~%" (tailleListe '(1 2 3 4)))
+(format t "(tailleListe '(1 (2 3) 4) : ~A ~%" (tailleListe '(1 (2 3) 4)))
+(format t "(tailleListe '(1 ((2 3) 4)) : ~A ~%" (tailleListe '(1 ((2 3) 4))))
+(format t "(tailleListe '((2 3) 4) : ~A ~%" (tailleListe '((2 3) 4)))
 
-;; Exo du dessus mais qui marche pour les sous listes.
-(defun tailleListe2 (liste)
-    (cond
-        (null liste)
+
+;; Iteratif
+(defun tailleListeIteratif (liste) ;; marche pas
+    (let ( (nombreElement 0) )
+        (dolist (elt liste)
+            (setf nombreElement (+ nombreElement 1) )
+        )
+        nombreElement
     )
 )
 
-(format t "(tailleListe '(1 (2 3) 4) : ~A ~%" (tailleListe '(1 (2 3) 4)))
+(format t "~%##### test tailleListeIteratif #####~%")
+(format t "(tailleListeIteratif '(1 2 3 4) : ~A ~%" (tailleListeIteratif '(1 2 3 4)))
+(format t "(tailleListeIteratif '(1 (2 3) 4) : ~A ~%" (tailleListeIteratif '(1 (2 3) 4)))
+(format t "(tailleListeIteratif '(1 ((2 3) 4)) : ~A ~%" (tailleListeIteratif '(1 ((2 3) 4))))
+(format t "(tailleListeIteratif '((2 3) 4) : ~A ~%" (tailleListeIteratif '((2 3) 4)))
 
+;; Exo du dessus mais qui marche pour les sous listes.
+(defun tailleListe2 (liste) ;; marche pas
+    (if (null liste)
+        0
+        (if (not (listp (car liste)))
+            (+ 1 (tailleListe2 (cdr liste)))
+            (tailleListe2 (car liste))
+        )
+    )
+)
+
+(format t "~%##### test tailleListe2 #####~%")
+(format t "(tailleListe2 '(1 2 3 4) : ~A ~%" (tailleListe2 '(1 2 3 4)))
+(format t "(tailleListe2 '(1 (2 3) 4) : ~A ~%" (tailleListe2 '(1 (2 3) 4)))
+(format t "(tailleListe2 '(1 ((2 3) 4)) : ~A ~%" (tailleListe2 '(1 ((2 3) 4))))
+(format t "(tailleListe2 '(1 (2 3)) : ~A ~%" (tailleListe2 '(1 (2 3))))
+(format t "(tailleListe2 '((2 3) 4) : ~A ~%" (tailleListe2 '((2 3) 4)))
+
+;; Retourner 
